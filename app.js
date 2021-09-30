34936c862b684ca2ca2a525c4db09970d72f7544
Quincy Stafford <xXQS316Xx@gmail.com>
2021-09-30 13:39:21 -0500

    deleted

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
 document.addEventListener("DOMContentLoaded", function(){  
+    var alert = document.getElementsByTagName("h3")
+    alert.addEventListener("click", function(){
+        alert('Accusation: I accuse Jane, with the paper clip in the copy room!')
+    })
 })
