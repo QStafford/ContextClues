5e5d58f6d343c4a6187b2fc19b8c9c52eedc29e8
Quincy Stafford <xXQS316Xx@gmail.com>
2021-09-30 13:51:12 -0500

    Adjusted

--- app.js
+++ app.js
@@ -1,17 +1,12 @@
-document.addEventListener("DOMContentLoaded", function(){  
+document.addEventListener("DOMContentLoaded", function () {
     var Accuse = document.createElement("h3")
     var accuseText = document.createTextNode('Accusation')
     Accuse.appendChild(accuseText)
     document.body.appendChild(Accuse)
+//Needs Loops and click event Listener
 
-    
-    if (num < 100) {
-        var num = 0
-        document.body.append(Accuse)
-        num++
-    }
     var friends = ['James', "Jamie", 'Jane', 'Johnson', 'Jonathan']
-    var location = ['Alabama', 'Georgia', 'Mississippi', 'Florida','Tennessee', 'New York', 'Texas', 'California', 'New Mexico', 'Nevada']
+    var location = ['Alabama', 'Georgia', 'Mississippi', 'Florida', 'Tennessee', 'New York', 'Texas', 'California', 'New Mexico', 'Nevada']
     var weapons = ['Knife', 'feather', 'paper clip', 'shoe', 'glasses', 'bookmark', 'book', 'pepper', 'salt shaker', 'jacket', 'straw', 'Big Mac', 'cardboard', 'Shopping cart', 'Mouse Trap', 'Slipper', 'Phone', 'Anime', 'BloodHound', 'Necklace']
-    
+
 })
