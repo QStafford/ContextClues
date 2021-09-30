33257f78a8ff1a72444f9c397e87bbd36f29e3bf
Quincy Stafford <xXQS316Xx@gmail.com>
2021-09-30 13:22:59 -0500

    added accusation

---
+++ app.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function(){
+    var Accuse = document.createElement("h3")
+    var accuseText = document.createTextNode('Accusation')
+    Accuse.appendChild(accuseText)
+    document.body.appendChild(Accuse)
+})
