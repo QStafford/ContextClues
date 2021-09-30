2d30c6238f0bf35bb4daf1725b0339de30b4594c
Quincy Stafford <xXQS316Xx@gmail.com>
2021-09-30 13:33:50 -0500

    Adjusted

--- app.js
+++ app.js
@@ -1,10 +1,2 @@
-document.addEventListener("DOMContentLoaded", function(){
-    var Accuse = document.createElement("h3")
-    var accuseText = document.createTextNode('Accusation')
-    Accuse.appendChild(accuseText)
-    document.body.appendChild(Accuse)
-
-    for (let i = 0; i < 100; i++) {
-        document.body.appendChild(Accuse);
-    }
+document.addEventListener("DOMContentLoaded", function(){  
 })
