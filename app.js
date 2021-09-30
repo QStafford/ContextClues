7a6c5f027a678b4fcd8aa3d32d46e718a36561e7
Quincy Stafford <xXQS316Xx@gmail.com>
2021-09-30 13:49:50 -0500

    added arrays

--- app.js
+++ app.js
@@ -3,4 +3,15 @@ document.addEventListener("DOMContentLoaded", function(){
     var accuseText = document.createTextNode('Accusation')
     Accuse.appendChild(accuseText)
     document.body.appendChild(Accuse)
+
+    
+    if (num < 100) {
+        var num = 0
+        document.body.append(Accuse)
+        num++
+    }
+    var friends = ['James', "Jamie", 'Jane', 'Johnson', 'Jonathan']
+    var location = ['Alabama', 'Georgia', 'Mississippi', 'Florida','Tennessee', 'New York', 'Texas', 'California', 'New Mexico', 'Nevada']
+    var weapons = ['Knife', 'feather', 'paper clip', 'shoe', 'glasses', 'bookmark', 'book', 'pepper', 'salt shaker', 'jacket', 'straw', 'Big Mac', 'cardboard', 'Shopping cart', 'Mouse Trap', 'Slipper', 'Phone', 'Anime', 'BloodHound', 'Necklace']
+    
 })
