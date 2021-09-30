bc648b4889de48737bd28db102430c83a0fde44b
Quincy Stafford <xXQS316Xx@gmail.com>
2021-09-30 13:57:54 -0500

    Adjusted

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ document.addEventListener("DOMContentLoaded", function () {
     Accuse.appendChild(accuseText)
     document.body.appendChild(Accuse)
 //Needs Loops and click event Listener
+    var i = 0
+    
+    while (i < 100) {
+        i++
+        document.body.append(Accuse)
+       console.log(i)
+    }
+
 
     var friends = ['James', "Jamie", 'Jane', 'Johnson', 'Jonathan']
     var location = ['Alabama', 'Georgia', 'Mississippi', 'Florida', 'Tennessee', 'New York', 'Texas', 'California', 'New Mexico', 'Nevada']
